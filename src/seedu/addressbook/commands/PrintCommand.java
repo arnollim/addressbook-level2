52a2582288075bc566ba2e2311254f5c4a8c6aa1
Arnold <arnol.ice@gmail.com>
2017-10-30 21:55:06 +0800

    add PrintCommand command

--- src/seedu/addressbook/commands/PrintCommand.java
+++ src/seedu/addressbook/commands/PrintCommand.java
@@ -1,4 +1,30 @@
 package seedu.addressbook.commands;
 
-public class PrintCommand {
+import java.io.IOException;
+import java.util.Arrays;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.charset.Charset;
+
+import java.util.List;
+
+public class PrintCommand extends Command {
+
+    public static String filename;
+
+    public PrintCommand(String filename) {this.filename = filename;}
+
+    @Override
+    public CommandResult execute(){
+        List<String> lines = Arrays.asList("The first line", "The second line");
+        Path file = Paths.get("addressbook.txt");
+        try {
+            Files.write(file, lines, Charset.forName("UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+       return new CommandResult("LOL");
+    }
+
 }
