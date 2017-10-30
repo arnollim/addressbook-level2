b649c4f71b8d1c280e72af898523c382e7d6a3b6
Arnold <arnol.ice@gmail.com>
2017-10-30 22:58:06 +0800

    Work on PrintCommand

--- src/seedu/addressbook/commands/PrintCommand.java
+++ src/seedu/addressbook/commands/PrintCommand.java
@@ -1,6 +1,7 @@
 package seedu.addressbook.commands;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,7 +10,15 @@ import java.nio.charset.Charset;
 
 import java.util.List;
 
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
 public class PrintCommand extends Command {
+
+    public static final String COMMAND_WORD = "print";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Saves the addressbook into a txt file for your viewing.\n"
+            + "Example: " + COMMAND_WORD;
 
     public static String filename;
 
@@ -17,7 +26,19 @@ public class PrintCommand extends Command {
 
     @Override
     public CommandResult execute(){
-        List<String> lines = Arrays.asList("The first line", "The second line");
+
+        List<ReadOnlyPerson> temp = new ArrayList<>();
+        List<String> lines = new ArrayList<>();
+        for (ReadOnlyPerson person: addressBook.getAllPersons()) {
+            String n = person.getName().fullName;
+            String e = person.getEmail().value;
+            String a = person.getAddress().value;
+            String entry = n + " " + e + " " + a;
+            lines.add(entry);
+            temp.add(person);
+        }
+
+        //List<String> lines = Arrays.asList("The first line", "The second line");
         Path file = Paths.get("addressbook.txt");
         try {
             Files.write(file, lines, Charset.forName("UTF-8"));
